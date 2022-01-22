522e527318c80fb5fc8b5c7ffdc0453288aa2c92
meipipo <meteor1038@gmail.com>
2022-01-22 16:01:16 -0500

    update map with Google Charts

--- emoji-sentiment/map/app.js
+++ emoji-sentiment/map/app.js
@@ -1,18 +1,38 @@
-
-const imgDir = 'imgs/'
-const numDays = 23;
-
+const numDays = 366;
 const daySlider = document.getElementById('day-slider');
 daySlider.setAttribute('max', numDays - 1);
+daySlider.addEventListener('input', drawMarkersMap);
 
-function inputChange(event) {
+google.charts.load('current', {
+    'packages': ['geochart'],
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+});
+google.charts.setOnLoadCallback(drawMarkersMap);
+
+const options = {
+    region: 'JP',
+    resolution: 'provinces',
+    datalessRegionColor: 'white',
+    backgroundColor: 'lightsteelblue',
+    colorAxis: {
+        colors: ['dodgerblue', 'white', 'tomato'],
+        minValue: 0.5,
+        maxValue: 3.5
+    },
+};
+
+function drawMarkersMap(event) {
     const day = Number(daySlider.value);
     const currentDate = new Date(2019, 9, 1);
     currentDate.setDate(currentDate.getDate() + day);
 
-    const imgPath = imgDir + currentDate.toISOString().substring(0, 10) + '-clr.png';
-    const img = document.getElementById('map-img');
-    img.setAttribute('src', imgPath);
+    const dateString = currentDate.toISOString().substring(0, 10);
+    var dateDisplay = document.getElementById('date-display');
+    dateDisplay.innerText = dateString;
+
+    var data = google.visualization.arrayToDataTable(ratioData[dateString]);
+
+    var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
 }
 
-daySlider.addEventListener('input', inputChange);
